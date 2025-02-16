888935ee87a5da84355ac0521bd962083d038a4a
Identity Theft <liam.fitz1920@gmail.com>
2025-02-16 20:24:37 +1100

    1.21.1

--- src/main/java/identitytheft/noxpanvils/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/identitytheft/noxpanvils/mixin/AnvilScreenHandlerMixin.java
@@ -1,5 +1,6 @@
 package identitytheft.noxpanvils.mixin;
 
+import net.minecraft.component.ComponentType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AnvilScreenHandler;
@@ -16,21 +17,36 @@ public abstract class AnvilScreenHandlerMixin {
 	@Shadow @Final private Property levelCost;
 
 	@Inject(method = "canTakeOutput", at = @At("HEAD"), cancellable = true)
-	private void noxpcost$canTakeOutput(PlayerEntity player, boolean present, CallbackInfoReturnable<Boolean> cir) {
+	private void noxpanvils$canTakeOutput(PlayerEntity player, boolean present, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(true);
 	}
 	@Inject(method = "onTakeOutput", at = @At("HEAD"))
-	private void noxpcost$onTakeOutput(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
+	private void noxpanvils$onTakeOutput(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
 		levelCost.set(0);
 	}
 
 	@Inject(method = "updateResult", at = @At("TAIL"))
-	private void norxpcost$updateResult(CallbackInfo ci) {
+	private void noxpanvils$updateResult(CallbackInfo ci) {
 		levelCost.set(0);
 	}
 
+	@Redirect(method = {"updateResult"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getOrDefault(Lnet/minecraft/component/ComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
+	private Object noxpanvils$getOrDefault(ItemStack instance, ComponentType componentType, Object o) {
+		return 0;
+	}
+
+	@Redirect(method = {"updateResult"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;get()I"))
+	private int noxpanvils$getLevelCost(Property instance) {
+		return 0;
+	}
+
+	@Inject(method = {"getNextCost"}, at = {@At("HEAD")}, cancellable = true)
+	private static void noxpanvils$getNextCost(int cost, CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(0);
+	}
+
     @ModifyConstant(method = "updateResult", constant = @Constant(intValue = 40, ordinal = 2))
-    private int noxpcost$maxValue(int input) {
+    private int noxpanvils$maxValue(int input) {
         return Integer.MAX_VALUE;
     }
 }
