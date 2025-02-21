94c3887b0af7c393be332f76b1a41a20a86aaa1e
Identity Theft <liam.fitz1920@gmail.com>
2025-02-21 15:40:44 +1100

    forge 1.20.1

--- src/main/java/identitytheft/noxpanvils/NoXpAnvils.java
+++ src/main/java/identitytheft/noxpanvils/NoXpAnvils.java
@@ -1,15 +1,48 @@
 package identitytheft.noxpanvils;
 
-import net.fabricmc.api.ModInitializer;
-
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class NoXpAnvils implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("no_xp_anvils");
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NoXpAnvils.MODID)
+public class NoXpAnvils
+{
+    public static final String MODID = "noxpanvils";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Starting No XP Anvils!");
-	}
+    public NoXpAnvils(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("Starting No XP Anvils!");
+    }
 }
--- src/main/java/identitytheft/noxpanvils/mixin/AnvilScreenMixin.java
+++ src/main/java/identitytheft/noxpanvils/mixin/AnvilScreenMixin.java
@@ -1,15 +1,15 @@
 package identitytheft.noxpanvils.mixin;
 
-import net.minecraft.client.gui.screen.ingame.AnvilScreen;
-import net.minecraft.text.MutableText;
+import net.minecraft.client.gui.screens.inventory.AnvilScreen;
+import net.minecraft.network.chat.MutableComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AnvilScreen.class)
 public class AnvilScreenMixin {
-	@Redirect(method = {"drawForeground"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/text/MutableText;"))
-	private MutableText noxpanvils$costText(String key, Object[] args) {
+	@Redirect(method = {"renderLabels"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/network/chat/MutableComponent;"))
+	public MutableComponent noxpanvils$costText(String pKey, Object[] pArgs) {
 		return null;
 	}
 }
